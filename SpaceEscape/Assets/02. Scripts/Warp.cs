e22e5c414a05febbc6d7569f1f94f0e72e4ead3b
kimyebeeen <hongsick0787@gmail.com>
2022-01-29 23:32:44 +0900

    warp스크립트 지연시간추가(22.01.29)
    warp 스크립트 지연 시간 추가했습니다.

--- SpaceEscape/Assets/02. Scripts/Warp.cs
+++ SpaceEscape/Assets/02. Scripts/Warp.cs
@@ -7,13 +7,19 @@ public class warp : MonoBehaviour
     public GameObject StartPos;
     public GameObject EndPos;
 
+    void warpRoutine()
+    {
+        StartPos.transform.position = EndPos.transform.position;
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
             StartPos = col.gameObject;
 
-            StartPos.transform.position = EndPos.transform.position;
+            //2초뒤 warp 실행
+            Invoke("warpRoutine", 2);
         }
     }
 
