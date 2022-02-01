d34db8066d401a85175a6a2ab6efa16eb93baf19
kimyebeeen <hongsick0787@gmail.com>
2022-02-02 00:52:21 +0900

    room1 objectdate 작성(22.02.02)

--- SpaceEscape/Assets/02. Scripts/Room1mini.cs
+++ SpaceEscape/Assets/02. Scripts/Room1mini.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Room1mini : MonoBehaviour
 {
-    public GameObject Player;
 
-    void Update()
+    public GameObject Player;
+    void Start()
     {
+        Player = GameObject.FindWithTag("PLAYER");
+    }
+    void Update ()
+    {
+        Vector3 dir;
+        //player와 서랍 사이의 거리=dir
+        dir = Player.transform.position - transform.position;
+
         //플레이어와 물체의 사이 거리가 5 이하이고, f 버튼을 누른다면
         if (Vector3.Distance(Player.transform.position, transform.position) < 5 && Input.GetKeyDown(KeyCode.F))
         {
-            Vector3 dir;
-            dir = Player.transform.position - transform.position;
+           
             //서랍을 연다.
             transform.position += Time.deltaTime * dir;
         }
+        
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            //서랍을 닫는다. 
+            transform.position -= Time.deltaTime * dir;
+        }
+        
     }
        
 }
